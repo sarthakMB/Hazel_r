e4266b872b171c1070a54795362a70b748068928
sarthakMNg <sarthak.barhanpure@gmail.com>
2022-12-18 11:05:13 +0530

    Setup basic application and entry points

---
+++ Hazel_p/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by Hazel Applications
+
+#include "Hazel_f/Application.h"
+#include <stdio.h>
+
+//--------Entry Point-----------
+#include "Hazel_f/EntryPoint.h"
+//------------------------------
